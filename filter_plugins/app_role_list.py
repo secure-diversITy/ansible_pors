3da659c8c4cb367fa038cba8e4660823043813d5
TF <mail@sedi.one>
2018-07-20 19:21:51 +0200

    fix: add missing app role gen filter plugins

---
+++ filter_plugins/app_role_list.py
@@ -0,0 +1,15 @@
+def app_role_list (app):
+ 
+  aignore = [ 'app.template','files','tasks','vars' ]
+  for lsapp in aignore: 
+	if lsapp in app: app.remove(lsapp)
+
+  rolepath = 'apps/'
+
+  app=[rolepath + role for role in app]
+   
+  return app
+
+class FilterModule(object):
+ def filters(self):
+   return {'app_role_list': app_role_list}
