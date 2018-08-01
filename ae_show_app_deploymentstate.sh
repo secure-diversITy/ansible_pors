5e69116af82c48f4156d7a66aa374d19e4552264
TF <mail@sedi.one>
2018-08-01 13:13:01 +0200

    initial ramp up for several playbooks:
    ae_add_app.yml (linking an app part1)
    ae_configure_ix_peernode.yml (add indexer peer node to a cluster)
    ae_deploy_systemconfigs.yml (push system/local configs)
    ae_download_openvmtools.yml (download openvmware-tools)
    ae_download_splunk.yml (download splunk)
    ae_enable_app_action.yml (linking an app part2)
    ae_get_targets.yml (generate list of targets)
    ae_install_openvmtools.yml (install openvmware-tools)
    ae_install_shelper.yml (install shelper)
    ae_install_splunk.yml (install splunk)
    ae_recreate_deploy.yml (autogenerate deploy playbook)
    ae_show_app_deploymentstate.sh (show deployed apps)
    ae_system_yum.yml (yum tasks, atm add elrepo kernel repo)
    ae_upgrade_shelper.yml (upgrade shelper)
    ae_upgrade_splunk.yml (upgrade splunk)

---
+++ ae_show_app_deploymentstate.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+##################################################################################
+#
+# Desc:		Show the deployed apps for each type
+# Copyright:	2017-2018, Thomas Fischer <mail -AT- sedi -DOT- one>
+#
+##################################################################################
+
+
+F_COLORS(){
+	cat <<EOFCOL
+
+    Colors and types:
+
+        ${BLUE}ansible hosted		The app is hosted on the ansible server (this server)
+                                Usually 3rd party apps$ENDC
+
+        ${MAGENTA}git hosted		The app is defined in the central git server
+                                Usually your / custom apps$ENDC
+
+        ${RED}DISABLED		Means the app is defined for this target group but
+                                the deployment is disabled (therefore the app is not
+                                installed on the target)$ENDC
+
+EOFCOL
+}
+
+F_USAGE(){
+	cat <<_EOUSAGE
+
+    Usage info:
+
+	-h|--help|help		this output
+        -dialog			machine friendly output
+	-env			the target environement (production/development/...)
+	<targetname>		a valid target name (group_vars/xxx)
+				you can define multiple target names separated by 
+				space and the whole list quoted
+				e.g. "dmc heavyforwarder"
+				if no targetname is given ALL targets are show
+	Examples:
+	
+	$0 -env production deploymentserver	(will show all apps for production deploymentserver group)
+	$0 -env production			(will show all apps for all production groups)
+	$0 -env development "deployer dmc"	(will show all apps for both deployer and dmc group in development)
+_EOUSAGE
+
+	F_COLORS
+}
+
+MF=0
+# check for calling
+while [ ! -z "$1" ];do
+    case "$1" in
+	-h|--help|help)
+	F_USAGE
+	exit
+	;;
+	-dialog)
+	MF=1
+	shift
+	;;
+	-logfile)
+	LOG="$2"
+	shift 2
+	;;
+	-env)
+	TARGETENV=${2}/group_vars
+	shift 2
+	;;
+	*)
+	TARGET="$1"
+	shift
+	;;
+    esac
+done
+
+[ -z "$TARGETENV" ] && TARGETENV=group_vars
+
+# define fancy color output
+  RED=$(tput setaf 1)
+  BLUE=$(tput setaf 4)
+  MAGENTA=$(tput setaf 5)
+  ENDC=$(tput sgr0)
+
+F_COLORS
+
+# do the magic
+if [ -z "$TARGET" ];then
+  echo -e "\nNo specific target group(s) given. Will show all (give me a second..)\n"
+    # walk through all var folders,
+    # catch all app definition files,
+    # check for enabled or disabled state
+    # write the list into APPLIST variable
+    APPLIST=$(for i in $(ls $TARGETENV/ |grep -v templates);do 
+	      echo -e "\nApps configured on $i";
+              for a in $(egrep -l '(bundle|git_repo)' $TARGETENV/$i/*);do
+	        egrep -ql "(\s+install:\s+[fF]alse)" $a
+	        if [ $? -eq 0 ];then
+	           echo "$RED${a##*/} (DISABLED)$ENDC"
+                else
+	          egrep -ql 'bundle' $a 
+	          if [ $? -eq 0 ];then
+                    echo "$BLUE${a##*/} (ansible hosted)$ENDC"
+                  else
+                    egrep -ql 'git_repo' $a
+                    if [ $? -eq 0 ];then
+                       echo "$MAGENTA${a##*/} (git hosted)$ENDC"
+		    fi
+                  fi
+	        fi
+	      done
+	     done)
+else
+ echo -e "\nOne or multiple target group(s) given."
+ for dir in $TARGET; do
+    echo "Processing $dir..."
+    if [ -d "$TARGETENV/$dir" ];then
+     APPLIST="${APPLIST}\n$(for i in $(ls $TARGETENV/ |grep $dir);do 
+              echo -e "\nApps configured on $i";
+              for a in $(egrep -l '(bundle|git_repo)' $TARGETENV/$i/*);do
+                egrep -ql "(\s+install:\s+[fF]alse)" $a
+                if [ $? -eq 0 ];then
+                   echo "$RED${a##*/} (DISABLED)$ENDC"
+                else
+                  egrep -ql 'bundle' $a 
+                  if [ $? -eq 0 ];then
+                    echo "$BLUE${a##*/} (ansible hosted)$ENDC"
+                  else
+                    egrep -ql 'git_repo' $a
+                    if [ $? -eq 0 ];then
+                       echo "$MAGENTA${a##*/} (git hosted)$ENDC"
+                    fi
+                  fi
+                fi
+              done
+             done)"
+    else
+      echo -e "\n${RED}**********************************************************\n\nTarget: >$TARGETENV/$dir< does not exists!!!\n\n**********************************************************$ENDC\n"
+    fi
+  done
+fi
+
+# Show the wonderful app list ;-)
+echo -e "$APPLIST\n\n"
+
