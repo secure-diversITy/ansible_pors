cb5ea359f8fdf46342e7064382394d4c4c277a86
TF <mail@sedi.one>
2021-01-23 18:32:28 +0100

    doc: change ref

--- docs/generate.sh
+++ docs/generate.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+###############################################################################################################
+# execute this from the ROOT of the repo!
+###########################################
+
 export PROJECT_NUMBER="$(git rev-parse HEAD ; git diff-index --quiet HEAD || echo '(with uncommitted changes)')"
 export PROJECT_NUMBER="Generated: $(date +%Y-%m-%d)"
 cd ./docs
 (exec doxygen)
 cd ../github.io/pors/
+git checkout master
 git status
-echo -e "\n\tcd github.io/pors/\n\tgit commit -m 'update documentation'\n\tgit push\n"
+echo -e "\n\tcd github.io/pors/\n\tgit add .\n\tgit commit -a -m 'update documentation'\n\tgit push\n"
