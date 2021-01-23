6990710565ae53f4c9e50fa31d5a03b6a1b064c3
TF <mail@sedi.one>
2021-01-23 18:36:50 +0100

    doc: complete instructions

--- docs/generate.sh
+++ docs/generate.sh
@@ -8,6 +8,9 @@ export PROJECT_NUMBER="Generated: $(date +%Y-%m-%d)"
 cd ./docs
 (exec doxygen)
 cd ../github.io/pors/
-git checkout master
+git checkout develop
 git status
 echo -e "\n\tcd github.io/pors/\n\tgit add .\n\tgit commit -a -m 'update documentation'\n\tgit push\n"
+git checkout master
+git merge develop
+git push
