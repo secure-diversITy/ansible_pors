c2d1566fdaef39e3d7a7be2f95546ea4931e0b12
TF <mail@sedi.one>
2021-01-25 13:14:02 +0100

    doc: change ref, change theme

--- docs/generate.sh
+++ docs/generate.sh
@@ -6,11 +6,11 @@
 export PROJECT_NUMBER="$(git rev-parse HEAD ; git diff-index --quiet HEAD || echo '(with uncommitted changes)')"
 export PROJECT_NUMBER="Generated: $(date +%Y-%m-%d)"
 cd ./docs
-(exec doxygen)
+(exec doxygen Doxyfile_extra)
 cd ../github.io/pors/
 git checkout develop
 git status
-echo -e "\n\tcd github.io/pors/\n\tgit add .\n\tgit commit -a -m 'update documentation'\n\tgit push\n"
-git checkout master
-git merge develop
-git push
+echo -e "\n\tcd github.io/pors/\n\tgit add .\n\tgit commit -a -m 'update documentation'"
+echo -e "\tgit checkout master"
+echo -e "\tgit merge develop"
+echo -e "\tgit push develop master"
